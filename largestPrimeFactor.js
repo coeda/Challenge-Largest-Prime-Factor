b367100df1c9bbdea745300e80d5cc5e5266724b
Casey Oeda <caseyoeda@Caseys-MacBook-Pro.local>
2016-09-08 15:06:14 -1000

    created while loop with nested while loop to divide n by all possible factors, attempting to find prime factors thats less than the square root of n

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,17 @@
 exports.largestPrimeFactor = function(n){
   var primeNumber = 0;
   // do your work here
-
+var divisor = 2;
+  //starts checking from 2
+while(n % divisor > 0){
+  //loops through numbers while n % divisor > 0
+  divisor ++;
+  //increments by 1 to check for highest prime factor
+  while(n % divisor === 0 && divisor < Math.sqrt(n)){
+  //if n is divisible by the divisor, and less than half of the squareroot of n, then divide n
+    n /= divisor;
+  }
+}
+  primeNumber = n;
   return primeNumber;
 };
